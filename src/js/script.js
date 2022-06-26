a90ccd5c1648268340a61863d29a35ff16e44a9e
Matheus M. de Santana <matheus.m.santana87@gmail.com>
2022-06-25 22:01:17 -0300

    entendendo eventListner

---
+++ src/js/script.js
@@ -0,0 +1,11 @@
+const td = document.querySelectorAll('td')
+
+const td1 = document.getElementById('td1') 
+
+td1.addEventListener('click', function(){
+    console.log('fui clicado')
+});
+
+td.addEventListener('click', function(){
+    console.log('fui clicado')
+});
